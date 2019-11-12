6ca42c83365ada82cd988206eb5a4b82c7f9cda4
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2019-11-12 09:32:03 +0000

    SQL query 2 done.

--- queries.sql
+++ queries.sql
@@ -5,10 +5,8 @@ WITH aux AS (
 	SELECT inc.item_id, COUNT(inc.anomalia_id) AS count
 	FROM proj.incidencia AS inc
 	GROUP BY inc.item_id
-), max_aux AS (
-	SELECT DISTINCT MAX(count) AS max_count
-	FROM aux
 )
+
 SELECT * 
 FROM proj.local_publico AS lp
 NATURAL JOIN (
@@ -17,11 +15,33 @@ NATURAL JOIN (
 	INNER JOIN (
 		SELECT item_id, count 
 		FROM aux
-		WHERE count = (SELECT max_count FROM max_aux)
+		WHERE count IN (SELECT MAX(count) FROM aux)
 	) AS max 
 	ON max.item_id = item.id
 ) AS coords;
 
+
 -- 2.
+WITH n AS (
+	SELECT email, COUNT(anomalia_id) count
+	FROM proj.incidencia AS inc
+	INNER JOIN ( 
+		SELECT id
+		FROM proj.anomalia
+		NATURAL JOIN proj.anomalia_traducao
+		WHERE ts BETWEEN '2019-06-01 00:00:00'::timestamp AND '2019-12-31 23:59:59'
+	) AS anom 
+	ON inc.anomalia_id = anom.id
+	GROUP BY email
+)
+
+SELECT * 
+FROM proj.utilizador u
+NATURAL JOIN proj.utilizador_regular ur 
+WHERE ur.email IN (
+	SELECT n.email 
+	FROM n 
+	WHERE count IN (SELECT MAX(count) FROM n)
+);
 -- 3.
 -- 4.
