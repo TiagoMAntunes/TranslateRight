7f0ce91219db33db437bd54a9a6afac825e9949c
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2019-11-18 17:23:04 +0000

    Changed data types.

--- schema.sql
+++ schema.sql
@@ -1,16 +1,27 @@
---DROP DATABASE translate_right;
---CREATE DATABASE translate_right;
+--DROP SCHEMA proj CASCADE;
+CREATE SCHEMA IF NOT EXISTS proj;
+/*
+DROP TABLE IF EXISTS
+	proj.local_publico,
+	proj.item,
+	proj.anomalia,
+	proj.anomalia_traducao,
+	proj.duplicado,
+	proj.utilizador,
+	proj.utilizador_regular,
+	proj.utilizador_qualificado,
+	proj.incidencia,
+	proj.correcao,
+	proj.proposta_de_correcao;
+*/
 
-DROP SCHEMA proj CASCADE;
-
-CREATE SCHEMA  IF NOT EXISTS proj;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA proj TO 
 	ist189504, ist189545, ist189469;
 
 CREATE TABLE proj.local_publico (
 	latitude numeric(8, 6) NOT NULL,
 	longitude numeric(9, 6) NOT NULL, 
-	nome char(50) NOT NULL,
+	nome varchar(120) NOT NULL,
 	CHECK(latitude >= -90 AND latitude <= 90),	
 	CHECK(longitude <= 180 AND longitude >= -180),
 	PRIMARY KEY(latitude, longitude)
@@ -18,8 +29,8 @@ CREATE TABLE proj.local_publico (
 
 CREATE TABLE proj.item (
 	id serial,
-	descricao char(180) NOT NULL,
-	localizacao char(30) NOT NULL,	-- <- TODO verify
+	descricao varchar(1024) NOT NULL,
+	localizacao varchar(1024) NOT NULL,	-- <- TODO verify
 	latitude numeric(8, 6) NOT NULL,
 	longitude numeric(9, 6) NOT NULL,
 	CHECK(latitude >= -90 AND latitude <= 90),
@@ -34,19 +45,19 @@ CREATE TABLE proj.item (
 CREATE TABLE proj.anomalia (
 	id serial, 
 	zona box NOT NULL, 	
-	imagem	char(100) NOT NULL,		-- link to image
-	lingua char(15) NOT NULL,
-	ts TIMESTAMP NOT NULL,
-	descricao char(180) NOT NULL,
-	tem_anomalia_traducao BOOLEAN NOT NULL,	
+	imagem bytea NOT NULL,		-- link to image
+	lingua varchar(120) NOT NULL,
+	ts timestamp NOT NULL,
+	descricao varchar(1024) NOT NULL,
+	tem_anomalia_traducao boolean NOT NULL,	
 	PRIMARY KEY(id)
 );
 
 
 CREATE TABLE proj.anomalia_traducao (	-- Beware of IC_1 and IC_2
-	id serial NOT NULL,	
+	id serial,	
 	zona2 box NOT NULL, 
-	lingua2 char(15) NOT NULL,
+	lingua2 varchar(120) NOT NULL,
 
 	FOREIGN KEY(id)
 		REFERENCES proj.anomalia
@@ -69,13 +80,13 @@ CREATE TABLE proj.duplicado (	-- Beware of IC_3
 );
 
 CREATE TABLE proj.utilizador (	-- Beware of IC_4
-	email char(50),
-	password char(25) NOT NULL,
+	email varchar(120),
+	password varchar(30) NOT NULL,
 	PRIMARY KEY(email)
 );
 
 CREATE TABLE proj.utilizador_qualificado (	-- Beware of IC_5
-	email char(50) NOT NULL,
+	email varchar(120) NOT NULL,
 
 	FOREIGN KEY(email)
 		REFERENCES proj.utilizador
@@ -84,7 +95,7 @@ CREATE TABLE proj.utilizador_qualificado (	-- Beware of IC_5
 );
 
 CREATE TABLE proj.utilizador_regular ( 	-- Beware of IC_6
-	email char(50) NOT NULL,
+	email varchar(120) NOT NULL,
 
 	FOREIGN KEY(email)
 		REFERENCES proj.utilizador
@@ -95,7 +106,7 @@ CREATE TABLE proj.utilizador_regular ( 	-- Beware of IC_6
 CREATE TABLE proj.incidencia (
 	anomalia_id integer NOT NULL, 
 	item_id integer NOT NULL,
-	email char(50) NOT NULL,
+	email varchar(120) NOT NULL,
 
 	FOREIGN KEY(anomalia_id)
 		REFERENCES proj.anomalia
@@ -110,10 +121,10 @@ CREATE TABLE proj.incidencia (
 );
 
 CREATE TABLE proj.proposta_de_correcao (		-- Beware of IC_7
-	email char(50) NOT NULL,
+	email varchar(120) NOT NULL,
 	nro integer NOT NULL,
-	data_hora TIMESTAMP NOT NULL,
-	texto VARCHAR(180) NOT NULL, 	-- It's possible to edit
+	data_hora timestamp NOT NULL,
+	texto varchar(1024) NOT NULL, 	-- It's possible to edit
 	CHECK(nro >= 0),
 	
 	FOREIGN KEY(email)
@@ -123,7 +134,7 @@ CREATE TABLE proj.proposta_de_correcao (		-- Beware of IC_7
 );
 
 CREATE TABLE proj.correcao (
-	email char(50) NOT NULL,
+	email varchar(120) NOT NULL,
 	nro integer NOT NULL,
 	anomalia_id integer NOT NULL,
 	
