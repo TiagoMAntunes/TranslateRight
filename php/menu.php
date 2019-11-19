62886bd4de7f9bb1d6f6e5f84c320dd41bf255c4
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2019-11-19 16:24:26 +0000

    Removed unnecessary code.

--- php/menu.php
+++ php/menu.php
@@ -1,24 +1,11 @@
 <html>
     <body>
         <?php
-            try {
-                $host = "yyyyyyyy" //db name
-                $user = "xxxxxxx"
-                $password = "xxxxxxx"
-                $dbname = $user
-
-                $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                $files = glob("*.php");
-                foreach ($files as $file) {
-                    echo("<h2><a href=\"{$file}\">");
-                    echo(substr($file,0, -4));
-                    echo("</a> </h2>\n");
-                }
-            }
-            catch(PDOException $e) {
-                echo("<p>ERROR: {$e->getMessage()}</p>");
+            $files = glob("*.php");
+            foreach ($files as $file) {
+                echo("<h2><a href=\"{$file}\">");
+                echo(substr($file,0, -4));
+                echo("</a> </h2>\n");
             }
         ?>
     </body>
