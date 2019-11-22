be5aaf2154eed840ca5e52665513925a0d53c14f
Tiago Antunes <tiago.melo.antunes@gmail.com>
2019-11-22 11:02:21 +0000

    Now lists users

--- php/listar_utilizadores.php
+++ php/listar_utilizadores.php
@@ -8,7 +8,20 @@
 	try {
 		$db = database_connect();
 		//Inserir código de manuseamento da base de dados
-		echo $db;
+		$sql = "SELECT email FROM utilizador;";
+		$result = $db->prepare($sql);
+		$result->execute();
+		$db = null;
+
+
+		echo ("<table border=\"1\">\n");
+		echo ("<tr><td>email</td></tr>\n");
+		foreach ($result as $row) {
+			echo ("<tr>\n");
+			echo ("<td>{$row['email']}</td>\n");
+			echo ("</tr>\n");
+		}
+		echo ("</table>\n");
 	} catch (PDOException $e) {
 		echo "<p><b>Error connecting to database:</b> {$e->getMessage()}</p>";
 	}
