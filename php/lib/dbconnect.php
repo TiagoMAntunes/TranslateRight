c838febd4653f9ff44c2e7c40d2fccebd0ce438a
Tiago Antunes <tiago.melo.antunes@gmail.com>
2019-11-22 11:47:58 +0000

    Now works in private server

--- php/lib/dbconnect.php
+++ php/lib/dbconnect.php
@@ -2,8 +2,8 @@
 
     function database_connect() {
         include 'login.php'; // gets $user and $pass varying from each pc
-        $host = "db.ist.utl.pt";
-        $dbname = $user;
+        $host = "localhost";
+        $dbname = "translateRight";
         $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
