849317de6f2453b8f7647b8e7c3fd69809ff22e5
joanaalvoeiro <joanaalvoeiro@gmail.com>
2019-12-06 19:09:36 +0000

    small changes to populate_star

--- populate_star.sql
+++ populate_star.sql
@@ -9,7 +9,7 @@ SELECT email, 'utilizador qualificado' FROM utilizador_qualificado;
 -- d_tempo(id_tempo, dia, dia_da_semana, semana, mes, trimestre, ano)
 
 INSERT INTO d_tempo(dia, dia_da_semana, semana, mes, trimestre, ano)
-SELECT extract(DAY FROM ts), extract(DOW FROM ts), extract(WEEK FROM ts),
+SELECT DISTINCT extract(DAY FROM ts), extract(DOW FROM ts), extract(WEEK FROM ts),
         extract(MONTH FROM ts), extract(QUARTER FROM ts), extract(YEAR FROM ts) FROM anomalia;
 
 
@@ -21,9 +21,16 @@ WITH items AS (
     WHERE item.id = i.item_id
 )
 INSERT INTO d_local(latitude, longitude, nome)
-SELECT latitude, longitude, localizacao FROM items;
+SELECT DISTINCT latitude, longitude, localizacao FROM items;
 
 -- d_lingua(id_lingua, lingua, país)
 
 INSERT INTO d_lingua(lingua)
-SELECT lingua FROM anomalia;
+SELECT DISTINCT lingua FROM anomalia;
+
+/* f_anomalia(id_ulizador, id_tempo, id_local, id_lingua, po_nomalia, com_proposta)
+- id_ulizador: FK(d_ulizador)
+- id_tempo: FK(d_tempo)
+- id_local: FK (d_local)
+- id_lingua: FK(d_lingua)*/
+
